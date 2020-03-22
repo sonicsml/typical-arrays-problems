9655f87b55ece694d1f61ea3034979b397e81c19
Polovkov Dmitry <sonics.scs@gmail.com>
2020-03-22 22:27:54 +0300

    feat: solution for task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
+
 exports.min = function min (array) {
-  return 0;
+  if ((array) && (this.length !== 0)) {
+    let minn = [0];
+    for (let maxItem of array) {
+      if (maxItem < minn ) {
+        minn = maxItem;
+      }
+    }
+    return minn;
+  } else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ((array) && (this.length !== 0)) {
+  let maxn = [0];
+  for (let maxItem of array) {
+    if (maxItem > maxn) {
+      maxn = maxItem;
+    }
+  }
+  return maxn;
+  } else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ((array) && (array.length != 0)) {
+    let result = array.reduce(function(sum, current) {
+      return sum + current;
+    }, 0);
+    console.log (result/array.length);
+    return (result/array.length);
+  } else return 0;
 }
